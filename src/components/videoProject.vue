5d61924b60db0bc8fa157c636dc6f50ba7e0e79d
sanzdecastro <sanzdecastro@gmail.com>
2024-09-16 21:16:02 +0200

    remove videojs

--- src/components/videoProject.vue
+++ src/components/videoProject.vue
@@ -59,9 +59,9 @@ export default {
 <template>
   
   <div class="media-container" :class="media.AspectRatio">
-    <video  ref="videoPlayer" class="vjs-matrix video-js " :poster=media.Media.data.attributes.previewUrl></video>
-    <!-- <video controls :src="media.Media.data.attributes.url" v-if="media.Media.data.attributes.ext === '.mp4' && media.autoplay != true" class="media-animation" :poster=media.Media.data.attributes.previewUrl></video>
-    <video autoplay playsinline controls loop controlsList="nodownload" :src="media.Media.data.attributes.url" v-if="media.Media.data.attributes.ext === '.mp4' && media.autoplay === true" class="media-animation"></video> -->
+    <!-- <video  ref="videoPlayer" class="vjs-matrix video-js " :poster=media.Media.data.attributes.previewUrl></video> -->
+    <video controls :src="media.Media.data.attributes.url" v-if="media.Media.data.attributes.ext === '.mp4' && media.autoplay != true" class="media-animation" :poster=media.Media.data.attributes.previewUrl></video>
+    <video autoplay playsinline controls loop controlsList="nodownload" :src="media.Media.data.attributes.url" v-if="media.Media.data.attributes.ext === '.mp4' && media.autoplay === true" class="media-animation"></video>
     <img :src="media.Media.data.attributes.url" v-if="media.Media.data.attributes.ext === '.jpg' || media.Media.data.attributes.ext === '.png'" class="media-animation">
   </div>
   <div v-if="media.Leyenda !== null" class="legend">{{ media.Leyenda }}</div>
