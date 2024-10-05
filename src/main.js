81edb2521fefc4a0a50e3c2675f40c7f07013e0e
sanzdecastro <sanzdecastro@gmail.com>
2024-10-05 14:46:21 +0200

    fix

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import { injectSpeedInsights } from "@vercel/speed-insights/vue"
 import { inject } from '@vercel/analytics';
 
 const app = createApp(App)
@@ -16,4 +15,3 @@ app.use(router)
 app.mount('#app')
 
 inject();
-injectSpeedInsights();
